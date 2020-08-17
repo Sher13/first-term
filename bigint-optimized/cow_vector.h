d6409c8308cf5c67453a68d386e1b57b3c33f89a
Sher13 <45572482+Sher13@users.noreply.github.com>
2020-08-17 22:51:13 +0300

    Revert "fix comments)"
    This reverts commit efa88333ff4bd7b2ed775e3dc6443367a3ce55d3.

--- bigint-optimized/cow_vector.h
+++ bigint-optimized/cow_vector.h
@@ -9,21 +9,21 @@ private:
 public:
 
     cow_vector()
-            : counter(1) {};
-
+            : counter(1){};
     cow_vector(size_t n)
-            : counter(1), data_(n) {};
-
+            :counter(1){
+        data_ = std::vector<uint32_t>(n);
+    };
     cow_vector(std::vector<uint32_t> const &b)
-            : counter(1), data_(b) {};
-
+            :counter(1){
+        data_ = std::vector<uint32_t>(b);
+    };
 
     ~cow_vector() {};
 
     void inc() {
         counter++;
     }
-
     void dec() {
         counter--;
     }
@@ -36,12 +36,12 @@ public:
         return counter == 1;
     };
 
-    std::vector<uint32_t> &data() {
+    std::vector<uint32_t>& data() {
         return data_;
     }
 
 
-    void swap(cow_vector &a, cow_vector &b) {
+    void swap(cow_vector& a, cow_vector& b) {
         using std::swap;
         swap(a.data_, b.data_);
         swap(a.counter, b.counter);
